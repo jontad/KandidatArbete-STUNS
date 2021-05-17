ea659eeef679ce3bc266d84964e53a52f525887e
vladislav <vladik@live.se>
2021-05-17 15:42:34 +0200

    filer lagda tillbaka, han_packet.c och makefile

---
+++ Smart Meter/han-meter-logger-master/han_packet.c
@@ -0,0 +1,132 @@
+#include "han_packet.h"
+#include <string.h>
+
+void raw_destroy(struct raw_packet_t* pack) {
+  free(pack->list_version);
+  free(pack->meter_id);
+  free(pack->meter_type);
+  free(pack->date);
+  free(pack);
+}
+
+char* raw_read_string(const char* buf, const int cur_pos, int* len) {
+  if (buf[cur_pos] != 0x0A) {
+    fprintf(stderr, "Field not a string! (type = 0x%02X)\n", buf[cur_pos]);
+    return 0;
+  }
+  uint8_t size = buf[cur_pos+1];
+  char* result = (char*) malloc((size + 2) * sizeof(char));
+
+  memset(result, 0, (size + 2) * sizeof(char));
+  
+  for (int i = cur_pos + 2; i - (cur_pos + 2) < size; i++) {
+    result[i - (cur_pos + 2)] = buf[i];
+  }
+
+  //memcpy(result, &buf[cur_pos + 2], size);
+
+  printf("Read string (%d bytes + 2): %s\n", size, result);
+  
+  *len = *len + size + 2;
+  return result;
+}
+
+int raw_skip_field(const char* buf, const int cur_pos) {
+  uint8_t size = buf[cur_pos + 1];
+  printf("Skipped %d + 2 bytes\n", size);
+  //printf("Skip 8 bytes");
+  return size + 2;
+  //return 8;
+}
+
+int raw_read_int(const char* buf, int cur_pos, void* result) {
+  uint8_t type = buf[cur_pos];
+  if (type == 0x06) { // 32-bit
+    uint32_t tmp = 0;
+    tmp |= (buf[cur_pos + 1] & 0xff) << 24;
+    tmp |= (buf[cur_pos + 2] & 0xff) << 16;
+    tmp |= (buf[cur_pos + 3] & 0xff) << 8;
+    tmp |= (buf[cur_pos + 4] & 0xff);
+    *((uint32_t*)result) = tmp;
+    printf("Read uint32_t: %d\n", *((uint32_t*)result));
+    return 5;
+  } else if (type == 0x12) {
+    uint16_t tmp = 0;
+    tmp |= (buf[cur_pos + 1] & 0xff) << 8;
+    tmp |= (buf[cur_pos + 2] & 0xff);
+    *((uint16_t*)result) = tmp;
+    printf("Read uint16_t: %d (0x%02X 0x%02X)\n", *((uint16_t*)result), buf[cur_pos + 1], buf[cur_pos + 2]);
+    return 3;
+  } else {
+    fprintf(stderr, "Field not a number! (type = 0x%02X)\n", type);
+    return -1;
+  }
+}
+
+int raw_read_float(const char * buf, int cur_pos, float* result) {
+  if (buf[cur_pos] != 0x06) {
+    fprintf(stderr, "Field not a float! (type = 0x%02X)\n", buf[cur_pos]);
+    return -1;
+  }
+  uint32_t raw_num = buf[cur_pos+1] << 24 | buf[cur_pos+2] << 16 | buf[cur_pos+3] << 8 | buf[cur_pos+4];
+  *result = raw_num / 100.0f;
+  printf("Read float: %f\n", *result);
+  return 5;
+}
+
+void raw_packet_parse(const char* buf, struct raw_packet_t* raw_pack) {
+  uint16_t year = 0 | (((uint16_t)buf[17] << 8) & 0xff00) | ((uint16_t)buf[18] & 0x00ff);
+  uint8_t month = buf[19];
+  uint8_t day = buf[20];
+  uint8_t hour = buf[22];
+  uint8_t minute = buf[23];
+  uint8_t second = buf[24];
+
+  printf("Date: %04d-%02d-%02d %02d:%02d:%02d\n", year, month, day, hour, minute, second);
+  raw_pack->date = (char*) malloc(32 * sizeof(char));
+  sprintf(raw_pack->date, "%04d-%02d-%02d %02d:%02d:%02d", year, month, day, hour, minute, second);
+  
+  uint8_t num_elems = buf[30];
+  printf("Number of elements: %d\n", num_elems);
+  int cur_pos = 31;
+  
+  //List version
+  raw_pack->list_version = raw_read_string(buf, cur_pos, &cur_pos);
+  printf("list_version (%p) = %s\n", raw_pack->list_version, raw_pack->list_version);
+  //OBIS for ID
+  cur_pos += raw_skip_field(buf, cur_pos);
+  raw_pack->meter_id = raw_read_string(buf, cur_pos, &cur_pos);
+  
+  cur_pos += raw_skip_field(buf, cur_pos);
+  raw_pack->meter_type = raw_read_string(buf, cur_pos, &cur_pos);
+
+  cur_pos += raw_skip_field(buf, cur_pos);
+  cur_pos += raw_read_int(buf, cur_pos, &raw_pack->active_power_p);
+
+  cur_pos += raw_skip_field(buf, cur_pos);
+  cur_pos += raw_read_int(buf, cur_pos, &raw_pack->active_power_n);
+
+  cur_pos += raw_skip_field(buf, cur_pos);
+  cur_pos += raw_read_int(buf, cur_pos, &raw_pack->reactive_power_p);
+
+  cur_pos += raw_skip_field(buf, cur_pos);
+  cur_pos += raw_read_int(buf, cur_pos, &raw_pack->reactive_power_n);
+
+  cur_pos += raw_skip_field(buf, cur_pos);
+  cur_pos += raw_read_float(buf, cur_pos, &raw_pack->i_l1);
+
+  cur_pos += raw_skip_field(buf, cur_pos);
+  cur_pos += raw_read_float(buf, cur_pos, &raw_pack->i_l2);
+
+  cur_pos += raw_skip_field(buf, cur_pos);
+  cur_pos += raw_read_float(buf, cur_pos, &raw_pack->i_l3);
+
+  cur_pos += raw_skip_field(buf, cur_pos);
+  cur_pos += raw_read_int(buf, cur_pos, &raw_pack->u_l1);
+
+  cur_pos += raw_skip_field(buf, cur_pos);
+  cur_pos += raw_read_int(buf, cur_pos, &raw_pack->u_l2);
+
+  cur_pos += raw_skip_field(buf, cur_pos);
+  cur_pos += raw_read_int(buf, cur_pos, &raw_pack->u_l3);
+}
