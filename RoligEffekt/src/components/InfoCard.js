9364e8830c6488bc265936479eb099b3bc9a4da5
jontad <47608480+jontad@users.noreply.github.com>
2021-05-30 16:56:08 +0200

    Fixed infobutton for class component

--- RoligEffekt/src/components/InfoCard.js
+++ RoligEffekt/src/components/InfoCard.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 import { StyleSheet } from 'react-native';
 
-import { Card } from "react-native-elements";
+import { Card } from 'react-native-elements';
 
 /*
 Single infocard displayed on home page 
@@ -9,44 +9,37 @@ that shows desired information
 */
 
 function InfoCard(props) {
-    
-   return(
-    <Card wrapperStyle={styles.wrapperStyle} containerStyle={styles.container}>
-        <Card.FeaturedTitle style={styles.headerText}>{props.headerText}</Card.FeaturedTitle>
-        {
-         <Card.FeaturedSubtitle style={styles.leftText}>{props.leftText}</Card.FeaturedSubtitle>        
-        }
-        <Card.FeaturedTitle style={styles.infoPosition}>{props.infoPosition}</Card.FeaturedTitle>
-        {
-         <Card.FeaturedSubtitle style={styles.rightText}>{props.rightText}</Card.FeaturedSubtitle>        
-        }
-        
-    </Card>
-   );
-    
+	return (
+		<Card wrapperStyle={styles.wrapperStyle} containerStyle={styles.container}>
+			<Card.FeaturedTitle style={styles.headerText}>{props.headerText}</Card.FeaturedTitle>
+			{<Card.FeaturedSubtitle style={styles.leftText}>{props.leftText}</Card.FeaturedSubtitle>}
+			<Card.FeaturedTitle style={styles.infoPosition}>{props.infoPosition}</Card.FeaturedTitle>
+			{<Card.FeaturedSubtitle style={styles.rightText}>{props.rightText}</Card.FeaturedSubtitle>}
+		</Card>
+	);
 }
 const styles = StyleSheet.create({
-    container: {
-        borderColor: 'rgba(158, 150, 150, .1)', 
-        borderRadius: 10, 
-    },
-    headerText: {
-        color: 'rgba(70, 70, 70, 1)'
-    },
-    leftText: {
-        color: 'rgba(70, 70, 70, 1)'
-    },
-    infoPosition: { 
-        position: 'absolute', 
-        right: -5, 
-        top: -8
-    },
-    rightText: {
-        color: 'rgba(70, 70, 70, 1)',  
-        position: 'absolute', 
-        right: 0, 
-        bottom: 0
-    },
-  });
+	container: {
+		borderColor: 'rgba(158, 150, 150, .1)',
+		borderRadius: 10,
+	},
+	headerText: {
+		color: 'rgba(70, 70, 70, 1)',
+	},
+	leftText: {
+		color: 'rgba(70, 70, 70, 1)',
+	},
+	infoPosition: {
+		position: 'absolute',
+		right: -5,
+		top: -8,
+	},
+	rightText: {
+		color: 'rgba(70, 70, 70, 1)',
+		position: 'absolute',
+		right: 0,
+		bottom: 0,
+	},
+});
 
 export default InfoCard;
--- RoligEffekt/src/components/LongButton.js
+++ RoligEffekt/src/components/LongButton.js
@@ -1,45 +1,42 @@
-import React from 'react'
+import React from 'react';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
-
 
 /*
 Horizontal button used in login/signup
 
 */
 
+const LongButton = (props) => (
+	<TouchableOpacity onPress={props.onPress} style={styles(props).button}>
+		<Text style={styles(props).text}>{props.title}</Text>
+	</TouchableOpacity>
+);
 
-const LongButton = (props) => 
-    <TouchableOpacity onPress={props.onPress} style={ styles(props).button }>
-        <Text style={ styles(props).text }>
-             {props.title}
-        </Text>
-    </TouchableOpacity>
-
-
-const styles = (props) => StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        paddingHorizontal: 10
-    },
-    button: {
-        alignItems: "center",
-        padding: '11px ',
-        backgroundColor: props.backgroundColor,
-        overflow: 'hidden', 
-        borderRadius: '100px',
-        position: 'absolute',
-        height: props.height,
-        left: props.left,
-        right: props.right,
-        bottom: props.bottom
-    },
-    text: {
-        textAlign: 'center',
-        fontWeight: 'bold',
-        fontSize: '20px',
-        color: props.textColor
-    },
-  });
+const styles = (props) =>
+	StyleSheet.create({
+		container: {
+			flex: 1,
+			justifyContent: 'center',
+			paddingHorizontal: 10,
+		},
+		button: {
+			alignItems: 'center',
+			padding: '11px ',
+			backgroundColor: props.backgroundColor,
+			overflow: 'hidden',
+			borderRadius: '100px',
+			position: 'absolute',
+			height: props.height,
+			left: props.left,
+			right: props.right,
+			bottom: props.bottom,
+		},
+		text: {
+			textAlign: 'center',
+			fontWeight: 'bold',
+			fontSize: '20px',
+			color: props.textColor,
+		},
+	});
 
 export default LongButton;
