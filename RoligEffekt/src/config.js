1c255c1fc2909dbbd1d61dbf51cd203f4a38c8e8
barwepaul <47776540+barwepaul@users.noreply.github.com>
2021-06-01 11:05:03 +0200

    Added comments

--- RoligEffekt/src/config.js
+++ RoligEffekt/src/config.js
@@ -1,11 +1,13 @@
 const prodConfig = {
 	baseUrl: '',
+	//Enter meter-ID here
 	meterID: '7340157011176133', 
 	production: true,
 };
 
 const localConfig = {
 	baseUrl: 'http://90.229.185.190:3000',
+	//Enter meter-ID here
 	meterID: '7340157011176133',
 	production: false,
 };
